36878f508e3141e5767c6843db681569401789b1
Muhammad Zuhdi <Muhammad.Zuhdi07@gmail.com>
2021-07-01 15:17:11 -0400

    initial commit

---
+++ springboot-backend/src/main/java/com/mzuhdi/springbootbackend/SpringbootBackendApplication.java
@@ -0,0 +1,22 @@
+package com.mzuhdi.springbootbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@SpringBootApplication
+@RestController
+public class SpringbootBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBackendApplication.class, args);
+	}
+	@GetMapping
+	public List<String> hello(){
+		return List.of("Hello", "World");
+	}
+
+}
